2ed893f3767d79fc2bf81c729518b49917352c27
asparlato13 <asparlato@loyola.edu>
2023-03-08 13:21:05 -0500

    Initial Commit

---
+++ ProjectJamz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectJamz
+//
+//  Created by Adrianna Parlato on 3/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
